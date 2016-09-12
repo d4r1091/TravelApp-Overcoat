6441af5428a8286dcbabedd49b1ecb84763b0158
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-09-13 01:01:44 +0200

    [networking] - added JSON Model Mappers classes

---
+++ FlightAppTest/Model/HotelModelMapper.h
@@ -0,0 +1,39 @@
+//
+//  HotelModelMapper.h
+//  FlightAppTest
+//
+//  Created by Dario Carlomagno on 13/09/16.
+//  Copyright © 2016 OnTheBeach. All rights reserved.
+//
+
+#import <Mantle/Mantle.h>
+
+/* Serializing this model
+ {
+ "name": "Labranda Isla Bonita Hotel",
+ "hotel_location": "Costa Adeje, Tenerife, Canaries",
+ "description": "On the Beach Offer - Save up to 15%\r\nBook this hotel by 31.10.2016, for travel between 01.11.2016 and 30.04.2017. Offer applicable to all room types on any board basis.",
+ "images": [
+ "SOME_IMAGE_URL",
+ "SOME_IMAGE_URL",
+ "SOME_IMAGE_URL"
+ ],
+ "rating": 4,
+ "facilities": [
+ "24 Hour Reception",
+ "Aerobics",
+ "Air Conditioning"
+ ]
+ }
+ */
+
+@interface HotelModelMapper : MTLModel <MTLJSONSerializing>
+
+@property (nonatomic, readonly, copy) NSString *name;
+@property (nonatomic, readonly, copy) NSString *hotelLocation;
+@property (nonatomic, readonly, copy) NSString *hotelDescription;
+@property (nonatomic, readonly, strong) NSArray *images;
+@property (nonatomic, assign, readonly) NSUInteger rating;
+@property (nonatomic, readonly, strong) NSArray *facilities;
+
+@end
