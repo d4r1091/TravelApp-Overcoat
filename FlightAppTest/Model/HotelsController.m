d1edc8dd650303c04bd84013efd7015e934808c5
Dario Carlomagno <dario.carlomagno@gmail.com>
2016-09-13 23:11:21 +0200

    [controller-ui] - filled both hotels and flights controllers

--- FlightAppTest/Model/HotelsController.m
+++ FlightAppTest/Model/HotelsController.m
@@ -7,7 +7,31 @@
 //
 
 #import "HotelsController.h"
+#import "APIClient.h"
+#import "HotelModelMapper.h"
+#import "Hotel.h"
 
 @implementation HotelsController
+
++ (void)getHotelsWithCompletionBlock:(void (^)(BOOL, NSArray *, NSError *))completionBlock {
+    [[APIClient sharedInstance] getHotelsWithCompletionBlock:^(BOOL success, NSArray *hotels, NSError *error) {
+        
+        NSMutableArray *hotelsObj = [NSMutableArray array];
+        if (success) {
+            for (HotelModelMapper *anHotel in hotels) {
+                Hotel *anHotelObj = [[Hotel alloc] initWithName:anHotel.name
+                                                  hotelLocation:anHotel.hotelLocation
+                                               hotelDescription:anHotel.hotelDescription
+                                                         images:anHotel.images
+                                                         rating:anHotel.rating
+                                                     facilities:anHotel.facilities];
+                
+                [hotelsObj addObject:anHotelObj];
+            }
+        }
+        completionBlock(success, hotelsObj, error);
+        
+    }];
+}
 
 @end
